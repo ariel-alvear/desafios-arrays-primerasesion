b67fafe6cc501604a19e7cbe896da5ee709ee826
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-28 15:44:23 -0300

    convertimos el código en un método y terminamos el desafío

--- filtro_procesos.rb
+++ filtro_procesos.rb
@@ -1,16 +1,17 @@
-data = open('procesos.data').readlines
-lines = data.count
-array = []
-lines.times do |i|
-    array << data[i].to_i
+def process_filter(file)
+    data = open(file).readlines
+    lines = data.count
+    array = []
+    lines.times do |i|
+        array << data[i].to_i
+    end
+
+    n = ARGV[0].to_i
+    new_array = array.select{ |x| x > n}
+
+    File.new("procesos_filtrados.data", "w")
+    File.write('procesos_filtrados.data', new_array.join(', '))
+
 end
 
-n = ARGV[0].to_i
-
-new_array = array.select{ |x| x > n}
-
-
-
-File.new("procesos_filtrados.data", "w")
-
-File.write('procesos_filtrados.data', new_array.join(', '))
+process_filter('procesos.data')
